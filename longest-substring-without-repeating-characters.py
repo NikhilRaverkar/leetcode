856cf6b680480c2aa1c6e6155acbe2fe3dee6721
Nikhil Raverkar <raverkar.nikhil@gmail.com>
2018-08-16 12:29:01 -0400

    updating a solution a bit

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -6,12 +6,11 @@ class Solution(object):
         while(i<len(s)):
             if ord(s[i]) not in charSet:
                 charSet[ord(s[i])]=i
-                length = len(charSet) if length < len(charSet) else length
                 i+=1
             else:
-                length=  len(charSet) if length<len(charSet) else length
                 i=charSet.get(ord(s[i]))+1
                 charSet.clear()
+            length = len(charSet) if length < len(charSet) else length
         return length
         """
         :type s: str
